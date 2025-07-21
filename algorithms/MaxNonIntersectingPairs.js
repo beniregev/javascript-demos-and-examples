bba9185a5a64dce2e2c2f17af2a9ad18721e7cf0
beniregev <beniregev@gmail.com>
2025-07-21 22:37:19 +0300

    Update solution for Algorithm Max Non-Intersecting Pairs

--- algorithms/MaxNonIntersectingPairs.js
+++ algorithms/MaxNonIntersectingPairs.js
@@ -1,34 +1,42 @@
 function maxPairs(arr) {
+    const freq = new Map();
+    for (let num of arr) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    const keys = Array.from(freq.keys());
+    const sumToPairs = new Map();
+
+    for (let i = 0; i < keys.length; i++) {
+        const a = keys[i];
+        const countA = freq.get(a);
+
+        // Pair a with a
+        const selfPairs = Math.floor(countA / 2);
+        if (selfPairs > 0) {
+            const sum = a + a;
+            sumToPairs.set(sum, (sumToPairs.get(sum) || 0) + selfPairs);
+        }
+
+        // Pair a with b where b > a
+        for (let j = i + 1; j < keys.length; j++) {
+            const b = keys[j];
+            const countB = freq.get(b);
+            const sum = a + b;
+            const pairCount = Math.min(countA, countB);
+            sumToPairs.set(sum, (sumToPairs.get(sum) || 0) + pairCount);
+        }
+    }
+
     let maxPairs = 0;
-
-    for (let target = 2; target <= 2000; target++) {
-        let freq = new Map();
-        for (let num of arr) {
-            freq.set(num, (freq.get(num) || 0) + 1);
-        }
-
-        let pairs = 0;
-        for (let num of arr) {
-            let complement = target - num;
-            if ((freq.get(num) || 0) > 0 && (freq.get(complement) || 0) > 0) {
-                if (num === complement && freq.get(num) >= 2) {
-                    freq.set(num, freq.get(num) - 2);
-                    pairs++;
-                } else if (num !== complement && freq.get(num) > 0 && freq.get(complement) > 0) {
-                    freq.set(num, freq.get(num) - 1);
-                    freq.set(complement, freq.get(complement) - 1);
-                    pairs++;
-                }
-            }
-        }
-
-        maxPairs = Math.max(maxPairs, pairs);
+    for (let val of sumToPairs.values()) {
+        maxPairs = Math.max(maxPairs, val);
     }
 
     return maxPairs;
 }
 
 // Test cases
-console.log(maxPairs([10, 1, 3, 1, 2, 2, 1, 0, 4])); // 3
-console.log(maxPairs([9, 9, 9, 9, 9]));              // 2
-console.log(maxPairs([1, 5, 3, 3, 4, 2]));           // 3
+console.log(maxPairs([10,1,3,1,2,2,1,0,4])); // 3
+console.log(maxPairs([9,9,9,9,9]));          // 2
+console.log(maxPairs([1,5,3,3,4,2]));        // 3
